9010e33e711e9a0604d3fccf507f0a295f408e12
Steven Moy <github@stevenmoy.com>
2016-07-01 13:55:32 -0700

    API is changed to return the entire MailMessage object.

---
+++ GmailApiTest.Tests/TestLatestMessage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AE.Net.Mail;
+using Google.Apis.Gmail.v1.Data;
+using NUnit.Framework;
+
+namespace GmailApiTest.Tests
+{
+  [TestFixture]
+  public class TestLatestMessage
+  {
+    const string FromEmailAddress = "stevenmoy.api.test@gmail.com";
+    const string ToEmailAddress = "stevenmoy.api.test+tag@gmail.com";
+    const string RelativePathForUserCreds = "google_api_test";
+    const string ClientSecretsFilename = "google_api_test_client_secret.json";
+
+    const string AddressWithTagFormat = "{0}{1}@{2}";
+
+    public TestLatestMessage ()
+    {
+    }
+
+    [Test]
+    public async Task TestMostRecent ()
+    {
+      await SendMessage (ToEmailAddress);
+      var relativePathForUserCreds = "google_api_test";
+      var clientSecretsFilename = "google_api_test_client_secret.json";
+      var five_minute_ago = DateTime.Now.Add (TimeSpan.FromMinutes (-5.0));
+      var message = await LatestMessageUtil.LatestMessage (relativePathForUserCreds, clientSecretsFilename, five_minute_ago);
+      Console.WriteLine (message);
+    }
+
+    /// <summary>
+    /// Intend is to make sure, we can get the latest message that 
+    /// matches the intended email address. So we make sure the global
+    /// latest email address is not the intended one, so we can check
+    /// if the functions returns the right message.
+    /// </summary>
+    /// <returns>The to email address matching.</returns>
+    [Test]
+    public async Task TestToEmailAddressMatching ()
+    {
+      // at t = 0, send a message
+      var taggedAddress = FormatEmailWithTag (FromEmailAddress, "+tag");
+      await SendMessage (taggedAddress);
+
+      // at t = 1, send a message
+      await SendMessage (FromEmailAddress);
+
+      var five_minute_ago = DateTime.Now.Add (TimeSpan.FromMinutes (-5.0));
+      var message = await LatestMessageUtil.LatestMessage (RelativePathForUserCreds, ClientSecretsFilename, five_minute_ago, taggedAddress);
+      Assert.AreEqual (taggedAddress, message.To.First ().Address);
+    }
+
+    public async Task SendMessage (string toEmailAddress)
+    {
+      var message = new MailMessage () {
+        Subject = "Unit test",
+        Body = "This is a test",
+        From = new System.Net.Mail.MailAddress (FromEmailAddress)
+      };
+      message.To.Add (new System.Net.Mail.MailAddress (toEmailAddress));
+      message.ReplyTo.Add (message.From);
+      var messageString = new StringWriter ();
+      message.Save (messageString);
+
+      var gmailService = await LatestMessageUtil.GetGmailService (RelativePathForUserCreds, ClientSecretsFilename, true);
+      gmailService.Users.Messages.Send (new Message {
+        Raw = LatestMessageUtil.Base64UrlEncode (messageString.ToString ())
+      }, "me").Execute ();
+    }
+
+    [Test]
+    public void TestFormatEmailWithTag ()
+    {
+      const string baseAddress = "hello@gmail.com";
+      const string addressWithTag = "hello+tag@gmail.com";
+      Assert.AreEqual (baseAddress, FormatEmailWithTag (baseAddress, string.Empty));
+      Assert.AreEqual (addressWithTag, FormatEmailWithTag (baseAddress, "+tag"));
+    }
+
+    public string FormatEmailWithTag (string baseEmailAddress, string tag)
+    {
+      var parts = baseEmailAddress.Split ('@');
+      var local = parts [0];
+      var domain = parts [1];
+      var tagPart = tag ?? string.Empty;
+      return string.Format (AddressWithTagFormat, local, tagPart, domain);
+    }
+  }
+}
+
--- GmailApiTest/LatestMessageUtil.cs
+++ GmailApiTest/LatestMessageUtil.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,17 +23,23 @@ namespace GmailApiTest
     {
     }
 
-    public static async Task<String> LatestMessage (string relativePathForUserCredentials, string clientSecretFileName, DateTime since)
+    public static async Task<GmailService> GetGmailService (string relativePathForUserCredentials, string clientSecretFileName, bool enableSending)
     {
       var homePath = Environment.GetEnvironmentVariable ("HOME");
-      var clientSecretPath = Path.Combine(homePath, relativePathForUserCredentials, clientSecretFileName);
+      var clientSecretPath = Path.Combine (homePath, relativePathForUserCredentials, clientSecretFileName);
       var storedUserCredentials = Path.Combine (homePath, relativePathForUserCredentials, "stored_user_creds");
 
       UserCredential credential;
+      var scopes = new List<string> ();
+      if (enableSending) {
+        scopes.Add (GmailService.Scope.GmailModify);
+      } else {
+        scopes.Add (GmailService.Scope.GmailReadonly);
+      }
       using (var stream = new FileStream (clientSecretPath, FileMode.Open, FileAccess.Read)) {
         credential = await GoogleWebAuthorizationBroker.AuthorizeAsync (GoogleClientSecrets.Load (stream).Secrets,
-            new [] { GmailService.Scope.GmailReadonly },
-            "user", CancellationToken.None, new LocalFileDataStore(storedUserCredentials));
+            scopes,
+            "user", CancellationToken.None, new LocalFileDataStore (storedUserCredentials));
       }
 
       var service = new GmailService (new BaseClientService.Initializer () {
@@ -39,17 +47,32 @@ namespace GmailApiTest
         ApplicationName = "Gmail Test",
       });
 
+      return service;
+    }
 
-      return await GetLatestMessageSince (service, since);
+    public static async Task<MailMessage> LatestMessage (
+      string relativePathForUserCredentials, 
+      string clientSecretFileName, 
+      DateTime since,
+      string toEmailAddress = null
+    )
+    {
+      var service = await LatestMessageUtil.GetGmailService (relativePathForUserCredentials, clientSecretFileName, false);
+      return await GetLatestMessageSince (service, since, toEmailAddress);
 
     }
 
-    public static async Task<string> GetLatestMessageSince (GmailService service, DateTime since)
+    public static async Task<MailMessage> GetLatestMessageSince (GmailService service, DateTime since, string toEmailAddress)
     {
       const int maxTries = 10;
       int numTries = 0;
       while (numTries < maxTries) {
-        var listMessageResponses = service.Users.Messages.List ("me").Execute ();
+        var listRequest = service.Users.Messages.List ("me");
+
+        if (!string.IsNullOrEmpty (toEmailAddress)) {
+          listRequest.Q = string.Format("to:{0}", toEmailAddress);
+        }
+        var listMessageResponses = listRequest.Execute ();
         if (listMessageResponses.Messages.Count > 0) {
           var getRequest = service.Users.Messages.Get ("me", listMessageResponses.Messages [0].Id);
           getRequest.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Raw;
@@ -58,16 +81,16 @@ namespace GmailApiTest
           var emailMessage = new MailMessage ();
           emailMessage.Load (decodedData);
           if (emailMessage.Date.Subtract (since).Seconds > 0) {
-            return emailMessage.Body;
-          } else {
-            numTries++;
-            await Task.Delay (5000);
+            return emailMessage;
           }
+
+          numTries++;
+          await Task.Delay (5000);
         }
 
       }
 
-      return string.Empty;
+      return null;
     }
 
     public static byte [] FromBase64ForUrlString (string base64ForUrlInput)
@@ -78,6 +101,16 @@ namespace GmailApiTest
       result.Replace ('-', '+');
       result.Replace ('_', '/');
       return Convert.FromBase64String (result.ToString ());
+    }
+
+    public static string Base64UrlEncode (string input)
+    {
+      var inputBytes = Encoding.UTF8.GetBytes (input);
+      // Special "url-safe" base64 encode.
+      return Convert.ToBase64String (inputBytes)
+        .Replace ('+', '-')
+        .Replace ('/', '_')
+        .Replace ("=", "");
     }
   }
 }
